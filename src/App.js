3d13029388a659d8d42544f1cfa90494293e46df
akshit sharma <akshitsharma20022@gmail.com>
2021-10-31 08:42:56 +0530

    updated favicon

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import About from './components/About';
 import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
   const [Mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null)
@@ -22,22 +28,34 @@ function App() {
       setMode('dark')
       document.body.style.backgroundColor = '#042743'
       showAlert("Dark Mode has been enabled", "success")
+      document.title = "TextUtils - Dark Mode"
       
     }
     else{ 
       setMode('light')
       document.body.style.backgroundColor = 'white'
       showAlert("Light Mode has been enabled", "success")
+      document.title = "TextUtils - Light Mode"
     } 
    }
   return (
     <>
+    <Router>
  <Navbar title = "textutils" about = "About Textutils" Mode = {Mode} toggleMode={toggleMode}/>
  <Alert Alert = {alert}/>
  <div className="container my-3">
- <TextForm showAlert={showAlert} heading = "Enter the text to analyze"  Mode = {Mode}/>
+ <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/">
+          <TextForm showAlert={showAlert} heading = "Enter the text to analyze"  Mode = {Mode}/>
+          </Route>
+        </Switch>
+ 
  {/* <About/> */}
  </div>
+ </Router>
  
     </>
   );
